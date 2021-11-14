15ff04ebad07d5d1a096347186803678fe8f4448
Ser <smurashov@.itpr.ru>
2021-11-14 19:55:54 +0300

    create security group

--- make-instances.tf
+++ make-instances.tf
@@ -3,11 +3,44 @@ provider "aws" {
   shared_credentials_file = "/home/ubuntu/.aws/credentials"
   profile = "default"
 }
+resource "aws_security_group" "allow_trafic" {
+  name        = "security-group1"
+  description = "Allow inbound traffic"
+
+
+  ingress {
+    description      = "SSH from VPC"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description      = "http from VPC"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "allow_ssh"
+  }
+}
 
 resource "aws_instance" "builder" {
   ami = "ami-0bd9c26722573e69b"
   instance_type = "t3.micro"
-  security_groups = ["sg-05252fc253494d7e5"]
+  vpc_security_group_ids = [aws_security_group.allow_trafic.id]
   key_name = "ser2"
   subnet_id = "subnet-d9825094"
   associate_public_ip_address = true
@@ -19,7 +52,7 @@ resource "aws_instance" "builder" {
 resource "aws_instance" "web" {
   ami = "ami-0bd9c26722573e69b"
   instance_type = "t3.micro"
-  security_groups = ["sg-05252fc253494d7e5"]
+  vpc_security_group_ids = [aws_security_group.allow_trafic.id]
   key_name = "ser2"
   subnet_id = "subnet-d9825094"
   associate_public_ip_address = true
